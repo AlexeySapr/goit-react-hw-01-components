8df155724adbac06de1f44b3e35f1a8ddccad390
Alexey Sapr <alexeysapr@gmail.com>
2021-12-08 16:56:18 +0200

    Add Statistics component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Profile from './components/social-profile/Profile';
 import user from './components/social-profile/user.json';
 
+import Statistics from './components/statistics/Statistics';
+import data from './components/statistics/data.json';
+
 export default function App() {
   return (
     <div>
@@ -11,6 +14,8 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 }
---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title = 'Upload stats', stats }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">{title}</h2>
+
+      <ul className="stat-list">
+        {stats.map(stat => {
+          return (
+            <li className="item" key={stat.id}>
+              <span className="label">{stat.label}</span>
+              <span className="percentage">{stat.percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+};
+
+export default Statistics;
