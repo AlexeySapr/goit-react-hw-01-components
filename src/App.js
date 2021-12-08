cfcdce9f5ca7c8e557769cbc8aadc289d545aa32
Alexey Sapr <alexeysapr@gmail.com>
2021-12-08 17:46:28 +0200

    Add FriendList component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import user from './components/social-profile/user.json';
 import Statistics from './components/statistics/Statistics';
 import data from './components/statistics/data.json';
 
+import FriendList from './components/friend-list/FriendList';
+import friends from './components/friend-list/friends.json';
+
 export default function App() {
   return (
     <div>
@@ -16,6 +19,7 @@ export default function App() {
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 }
