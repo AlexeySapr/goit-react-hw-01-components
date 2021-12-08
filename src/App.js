1e06eccdcf3554a069aba278e5ae8da30ceb90c6
Alexey Sapr <alexeysapr@gmail.com>
2021-12-08 15:31:16 +0200

    Add Profile component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from './components/social-profile/Profile';
+import user from './components/social-profile/user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const elem1 = <span>Hello</span>;
-const elem2 = <span> World</span>;
-
-const mainElement = (
-  <div>
-    {elem1}
-    {elem2}
-  </div>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-ReactDOM.render(mainElement, document.querySelector('#root'));
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
